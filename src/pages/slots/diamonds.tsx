4a74be47735303b92520b7102dcb6a0a748ef982
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2024-01-04 17:21:03 +0200

    height fix

--- src/pages/slots/diamonds.tsx
+++ src/pages/slots/diamonds.tsx
@@ -99,7 +99,7 @@ const Diamonds = () => {
 	return (
 		<div>
 			<Layout>
-				<div className='flex flex-col items-center justify-center h-[60vh]'>
+				<div className='flex flex-col items-center justify-center h-[80vh]'>
 					<div className='game border-2 rounded-md w-9/12 md:w-4/12 py-12 md:py-16 flex justify-center md:h-8/12'>
 						<div className='text-5xl md:text-6xl flex justify-center  items-center border-red-600 border-t-2 border-b-2 py-3'>
 							<span className='p-1'>{item1}</span>
