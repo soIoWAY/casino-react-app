c462f3ece0337d2f347a090a02336a106c90c698
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2024-01-08 18:49:16 +0200

    refactoring

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import FooterNav from './FooterNav'
 
 const Footer = () => {
 	return (
-		<div className='bg-[#1C1632] w-full px-32 py-16 h-full mt-4 shadow-md'>
+		<div className='bg-[#1C1632] w-full px-16 py-8 md:px-32 md:py-16 h-full mt-4 shadow-md'>
 			<FooterNav />
 		</div>
 	)
--- src/components/footer/FooterNav.tsx
+++ src/components/footer/FooterNav.tsx
@@ -1,54 +1,59 @@
+const cmnStyles = 'p-2 mt-5 flex justify-center'
+const cmnStylesText = 'text-center md:text-left'
+const cmnStylesH3 = 'uppercase mb-4 font-bold text-lg tracking-wide'
+
 const FooterNav = () => {
 	return (
-		<div className='flex flex-wrap md:justify-between'>
-			<div className='p-2 mt-5'>
-				<h3 className='uppercase mb-4 font-bold text-lg tracking-wide'>
-					Про нас
-				</h3>
-				<ul>
-					<li>Про нас</li>
-					<li>Договір оферти</li>
-				</ul>
+		// flex flex-wrap md:justify-between
+		<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 text-center'>
+			<div className={cmnStyles}>
+				<div className={cmnStylesText}>
+					<h3 className={cmnStylesH3}>Про нас</h3>
+					<ul>
+						<li>Про нас</li>
+						<li>Договір оферти</li>
+					</ul>
+				</div>
 			</div>
-			<div className='p-2 mt-5'>
-				<h3 className='uppercase mb-4 font-bold text-lg tracking-wide'>
-					Розділи
-				</h3>
-				<ul>
-					<li>Ставки на спорт</li>
-					<li>Ставки на футбол</li>
-					<li>Казино онлайн</li>
-					<li>Акції</li>
-				</ul>
+			<div className={cmnStyles}>
+				<div className={cmnStylesText}>
+					<h3 className={cmnStylesH3}>Розділи</h3>
+					<ul>
+						<li>Ставки на спорт</li>
+						<li>Ставки на футбол</li>
+						<li>Казино онлайн</li>
+						<li>Акції</li>
+					</ul>
+				</div>
 			</div>
-			<div className='p-2 mt-5'>
-				<h3 className='uppercase mb-4 font-bold text-lg tracking-wide'>
-					допомога
-				</h3>
-				<ul>
-					<li>Калькулятор систем</li>
-					<li>Поповнення/Виведення рахунку</li>
-				</ul>
+			<div className={cmnStyles}>
+				<div className={cmnStylesText}>
+					<h3 className={cmnStylesH3}>допомога</h3>
+					<ul>
+						<li>Калькулятор систем</li>
+						<li>Поповнення/Виведення рахунку</li>
+					</ul>
+				</div>
 			</div>
-			<div className='p-2 mt-5'>
-				<h3 className='uppercase mb-4 font-bold text-lg tracking-wide'>
-					Правила
-				</h3>
-				<ul>
-					<li>Правила організатора</li>
-					<li>Ліцензія</li>
-					<li>Відповідальна гра</li>
-					<li>Програма лояльності</li>
-				</ul>
+			<div className={cmnStyles}>
+				<div className={cmnStylesText}>
+					<h3 className={cmnStylesH3}>Правила</h3>
+					<ul>
+						<li>Правила організатора</li>
+						<li>Ліцензія</li>
+						<li>Відповідальна гра</li>
+						<li>Програма лояльності</li>
+					</ul>
+				</div>
 			</div>
-			<div className='p-2 mt-5'>
-				<h3 className='uppercase mb-4 font-bold text-lg tracking-wide'>
-					Контакти
-				</h3>
-				<ul>
-					<li>support@7kings.ua</li>
-					<li>Вакансії</li>
-				</ul>
+			<div className={cmnStyles}>
+				<div className={cmnStylesText}>
+					<h3 className={cmnStylesH3}>Контакти</h3>
+					<ul>
+						<li>support@7kings.ua</li>
+						<li>Вакансії</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	)
