7b3f4e10e8dc5f62d95b392b048edee7fe19bdc1
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2024-01-20 16:57:27 +0200

    test

--- src/components/header/DepositModal.tsx
+++ src/components/header/DepositModal.tsx
@@ -23,7 +23,7 @@ const DepositModal = ({ closeModal }: IDepositModal) => {
 
 	return (
 		<div className='fixed top-0 left-0 bg-black bg-opacity-75 h-screen w-full flex items-center justify-center'>
-			<div className='bg-[#0f051e] mx-auto flex flex-col h-auto px-3 py-4 lg:w-[35%] md:w-[55%] sm:w-[70%] w-[90%]'>
+			<div className='bg-[#0f051e] mx-auto flex flex-col min-h-screen sm:h-4/6 px-3 lg:w-[35%] md:w-[55%] sm:w-[70%] w-[90%]'>
 				<div className='flex justify-end mt-1'>
 					<button className='text-xl' onClick={closeModal}>
 						x
